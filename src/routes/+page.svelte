2adfd466f6afdd3a3755fe07de0a0733beb33ac9
Creaperhunter <creaperhunter531@gmail.com>
2022-11-12 21:14:48 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	let count = 0;
+	function inc() {
+		count += 1;
+	}
+</script>
+
+<h1>you clicked the button {count} times</h1>
+
+{#if count > 5}
+	<h1>you clicked the button more than 5 times</h1>
+{/if}
+<button on:click={inc}>click</button>
